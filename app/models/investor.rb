5dc6d9b6eda26bae3dea731a3b77fd25e6740a05
John Lannon <jlannon@gmail.com>
2022-03-15 10:38:58 -0500

    Add investor document storage
    Use ActiveStorage to store a document with each investor.
    Require presence of document.

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -1,11 +1,12 @@
 class Investor < ApplicationRecord
   REQUIRED_ATTRIBUTES = %i[
     address_city address_state address_street address_zipcode date_of_birth
-    first_name last_name phone_number social_security_number
+    document first_name last_name phone_number social_security_number
   ].freeze
 
   validates *REQUIRED_ATTRIBUTES, presence: true
   validates :social_security_number, uniqueness: true
+  has_one_attached :document
 
   belongs_to :user, inverse_of: 'investors'
 
--- spec/models/investor_spec.rb
+++ spec/models/investor_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Investor, type: :model do
     it { should validate_presence_of(:address_street) }
     it { should validate_presence_of(:address_zipcode) }
     it { should validate_presence_of(:date_of_birth) }
+    it { should validate_presence_of(:document) }
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
     it { should validate_presence_of(:phone_number) }
